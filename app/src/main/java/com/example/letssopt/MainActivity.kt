dbc71fc7e97ea3e622d491d502c9414afd07dac7
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-04-08 23:29:18 +0900

    add: #2 글꼴 설정 및 SignUpActivity 생성

--- app/src/main/java/com/example/letssopt/MainActivity.kt
+++ app/src/main/java/com/example/letssopt/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.letssopt.ui.theme.LETSSOPTTheme
 
@@ -32,10 +36,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column() {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+
+        // Font Family
+        Text(
+            text = "미리보기",
+            fontFamily = FontFamily(Font(R.font.pretendard_bold)),
+            fontWeight = FontWeight.Bold,
+            modifier = modifier
+        )
+
+        Text(
+            text = "미리보기",
+            fontFamily = FontFamily(Font(R.font.pretendard_regular)),
+            fontWeight = FontWeight.Normal,
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
