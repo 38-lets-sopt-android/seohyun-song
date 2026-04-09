700442dfc24dbf9d3ccc3cf86b9e0125ef0289d9
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-04-10 00:34:13 +0900

    feat: #2 로그인 화면 구현, 회원가입 화면 연결

--- app/src/main/java/com/example/letssopt/MainActivity.kt
+++ app/src/main/java/com/example/letssopt/MainActivity.kt
@@ -1,21 +1,44 @@
 package com.example.letssopt
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialogDefaults.containerColor
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.letssopt.ui.theme.LETSSOPTTheme
+import kotlin.jvm.java
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +47,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             LETSSOPTTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    LoginScreen(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -35,26 +58,142 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column() {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
+fun LoginScreen(name: String, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    var emailInput by remember { mutableStateOf("") }
+    var pwInput by remember { mutableStateOf("") }
 
-        // Font Family
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.Black)
+            .padding(all = 20.dp),
+    ) {
+        // 로고
         Text(
-            text = "미리보기",
-            fontFamily = FontFamily(Font(R.font.pretendard_bold)),
+            text = "watcha",
+            fontSize = 36.sp,
             fontWeight = FontWeight.Bold,
-            modifier = modifier
+            fontFamily = FontFamily(Font(R.font.pretendard_bold)),
+            color = Color(0xFFE8003C),
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(top = 60.dp),
         )
 
-        Text(
-            text = "미리보기",
+        // 이메일로 로그인
+        Text (
+            text = "이메일로 로그인",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily(Font(R.font.pretendard_bold)),
+            color = Color.White,
+            modifier = Modifier
+                .align(Alignment.Start)
+                .padding(top = 26.dp),
+        )
+
+        // 이메일
+        Text (
+            text = "이메일",
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Medium,
             fontFamily = FontFamily(Font(R.font.pretendard_regular)),
-            fontWeight = FontWeight.Normal,
-            modifier = modifier
+            color = Color(0xFF999999),
+            modifier = Modifier
+                .align(Alignment.Start)
+                .padding(top = 36.dp),
+        )
+
+        Spacer(modifier = Modifier.height(3.dp))
+
+        TextField (
+            value = emailInput,
+            onValueChange = { emailInput = it },
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(8.dp),
+            textStyle = TextStyle(
+                fontFamily = FontFamily(Font(R.font.pretendard_regular)),
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Normal,
+                color = Color.White
+            ),
+            placeholder = { Text(
+                text = "이메일 주소를 입력하세요",
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Medium,
+                fontFamily = FontFamily(Font(R.font.pretendard_regular)),
+                color = Color(0xFF666666),
+            ) },
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color(0xFF2A2A2A),
+                unfocusedContainerColor = Color(0xFF2A2A2A),
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            )
+        )
+
+        Spacer(modifier = Modifier.height(18.dp))
+
+        // 비밀번호
+        Text (
+            text = "비밀번호",
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Medium,
+            fontFamily = FontFamily(Font(R.font.pretendard_regular)),
+            color = Color(0xFF999999),
+            modifier = Modifier
+                .align(Alignment.Start)
+                //.padding(top = 36.dp),
+        )
+
+        Spacer(modifier = Modifier.height(3.dp))
+
+        TextField (
+            value = pwInput,
+            onValueChange = { pwInput = it },
+            modifier = Modifier.fillMaxWidth(),
+            visualTransformation = PasswordVisualTransformation(),
+            shape = RoundedCornerShape(8.dp),
+            textStyle = TextStyle(
+                fontFamily = FontFamily(Font(R.font.pretendard_regular)),
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Normal,
+                color = Color.White
+            ),
+            placeholder = { Text(
+                text = "비밀번호를 입력하세요",
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Medium,
+                fontFamily = FontFamily(Font(R.font.pretendard_regular)),
+                color = Color(0xFF666666),
+            ) },
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color(0xFF2A2A2A),
+                unfocusedContainerColor = Color(0xFF2A2A2A),
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            )
+        )
+
+        // Spacer(modifier = Modifier.height(333.dp))
+
+        // 회원가입 이동 텍스트
+        Text(
+            text = "아직 계정이 없으신가요? 회원가입",
+            fontSize = 14.sp,
+            fontWeight = FontWeight.W400,
+            fontFamily = FontFamily(Font(R.font.pretendard_regular)),
+            color = Color(0xFF999999),
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(top = 200.dp)
+                .clickable(onClick = {
+                    val intent = Intent(context,SignUpActivity::class.java)
+                    context.startActivity(intent)
+                })
         )
     }
 }
@@ -63,6 +202,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     LETSSOPTTheme {
-        Greeting("Android")
+        LoginScreen("Android")
     }
 }
--- app/src/main/java/com/example/letssopt/SignUpActivity.kt
+++ app/src/main/java/com/example/letssopt/SignUpActivity.kt
@@ -1,2 +1,48 @@
 package com.example.letssopt
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.letssopt.ui.theme.LETSSOPTTheme
+
+class SignUpActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LETSSOPTTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    SignUpScreen(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SignUpScreen(name: String, modifier: Modifier) {
+    Text(
+        "회원가입 화면",
+        modifier = Modifier.padding(top = 50.dp)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SignUpPreview() {
+    LETSSOPTTheme {
+        SignUpScreen("Android", modifier = Modifier)
+    }
+}
