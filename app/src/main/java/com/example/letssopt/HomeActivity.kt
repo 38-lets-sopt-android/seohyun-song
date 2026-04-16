e53daa6c0f0e312aa2977a25bfa09d8239f078d9
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-04-16 16:20:27 +0900

    feat: #6 왓챠 파티 LazyRow 구현

--- app/src/main/java/com/example/letssopt/HomeActivity.kt
+++ app/src/main/java/com/example/letssopt/HomeActivity.kt
@@ -1,12 +1,14 @@
 package com.example.letssopt
 
 import android.os.Bundle
+import android.text.Layout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -21,6 +23,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.BottomAppBar
@@ -78,6 +81,7 @@ fun HomeScreen(
     val scrollState = rememberScrollState()
     val newContentsDisplay = viewModel.contents.take(3)
     val watGorithm = viewModel.contents.drop(3).take(4)
+    val watchaParty = viewModel.contents.drop(7).take(2)
 
     Scaffold(
         topBar = {
@@ -266,6 +270,44 @@ fun HomeScreen(
                     )
                 }
             }
+            Spacer(Modifier.height(26.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "왓챠 파티",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    fontFamily = FontFamily(Font(R.font.pretendard_bold)),
+                    color = Color.White,
+                )
+                Text(
+                    text = "더보기",
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Normal,
+                    fontFamily = FontFamily(Font(R.font.pretendard_regular)),
+                    color = Color(0xFF999999)
+                )
+            }
+            Spacer(Modifier.height(8.dp))
+            LazyRow(
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                contentPadding = PaddingValues(horizontal = 8.dp),
+            ) {
+                items(watchaParty) {
+                    content ->
+                    WatchaPartyItem (
+                        title = content.title,
+                        time = content.time,
+                        imageRes = content.imageRes
+                    )
+                }
+            }
+            Spacer(Modifier.height(16.dp))
         }
     }
 }
@@ -302,6 +344,61 @@ fun ContentItem(
             .height(150.dp)
             .clip(RoundedCornerShape(10.dp)),
     )
+}
+
+@Composable
+fun WatchaPartyItem (
+    title: String,
+    time: String,
+    imageRes: Int,
+    modifier: Modifier = Modifier
+) {
+    Box() {
+        Column(
+            modifier = Modifier
+                .background(Color(0xFF2A2A2A)),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Image(
+                painter = painterResource(id = imageRes),
+                contentDescription = title,
+                modifier = Modifier
+                    .width(196.dp)
+                    .height(139.dp),
+            )
+            Text(
+                text = time,
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Medium,
+                fontFamily = FontFamily(Font(R.font.pretendard_regular)),
+                color = Color(0xFFE8003C),
+                modifier = Modifier.padding(start = 8.dp)
+            )
+            Text(
+                text = "# $title",
+                fontSize = 12.sp,
+                fontWeight = FontWeight.SemiBold,
+                fontFamily = FontFamily(Font(R.font.pretendard_regular)),
+                color = Color.White,
+                modifier = Modifier.padding(start = 8.dp)
+            )
+        }
+        Box(
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(top = 7.dp, end = 5.dp)
+                .size(35.dp)
+                .clip(CircleShape)
+                .background(Color.White),
+            contentAlignment = Alignment.Center
+            ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_noti_black),
+                contentDescription = "알림",
+                modifier = Modifier.size(18.dp),
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
