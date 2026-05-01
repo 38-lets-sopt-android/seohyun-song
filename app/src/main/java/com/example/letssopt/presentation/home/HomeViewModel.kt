c0791605a5d58cc47b4d4b07f67abb3c3b0d4988
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-05-01 12:39:59 +0900

    refactor: #9 HomeViewModel MVVM에 맞게 수정, 파일 디렉토리 변경

---
+++ app/src/main/java/com/example/letssopt/presentation/home/HomeViewModel.kt
@@ -0,0 +1,46 @@
+package com.example.letssopt
+
+import androidx.lifecycle.ViewModel
+import com.example.letssopt.activity.ContentItem
+import com.example.letssopt.activity.WatchaPartyItem
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class HomeViewModel : ViewModel() {
+    private val _newContents = MutableStateFlow<List<ContentItem>>(emptyList())
+    val newContents: StateFlow<List<ContentItem>> = _newContents.asStateFlow()
+
+    private val _watGorithm = MutableStateFlow<List<ContentItem>>(emptyList())
+    val watGorithm: StateFlow<List<ContentItem>> = _watGorithm.asStateFlow()
+
+    private val _upComing = MutableStateFlow<List<ContentItem>>(emptyList())
+    val upComing: StateFlow<List<ContentItem>> = _upComing.asStateFlow()
+
+    private val _watchaParty = MutableStateFlow<List<WatchaPartyItem>>(emptyList())
+    val watchaParty: StateFlow<List<WatchaPartyItem>> = _watchaParty.asStateFlow()
+
+    init {
+        _newContents.value = listOf(
+            ContentItem(title = "크라임씬", imageRes = R.drawable.img_crime_scene),
+            ContentItem(title = "크라임씬", imageRes = R.drawable.img_crime_scene),
+            ContentItem(title = "크라임씬", imageRes = R.drawable.img_crime_scene)
+        )
+        _watGorithm.value = listOf(
+            ContentItem(title = "이 사랑 통역 되나요?", imageRes = R.drawable.img_love_traslate),
+            ContentItem(title = "STRANGER THINGS 5", imageRes = R.drawable.img_stranger_things5),
+            ContentItem(title = "HALI MARY", imageRes = R.drawable.img_hailmary),
+            ContentItem(title = "이 사랑 통역 되나요?", imageRes = R.drawable.img_love_traslate)
+        )
+        _upComing.value = listOf(
+            ContentItem(title = "이 사랑 통역 되나요?", imageRes = R.drawable.img_love_traslate),
+            ContentItem(title = "STRANGER THINGS 5", imageRes = R.drawable.img_stranger_things5),
+            ContentItem(title = "HALI MARY", imageRes = R.drawable.img_hailmary),
+            ContentItem(title = "이 사랑 통역 되나요?", imageRes = R.drawable.img_love_traslate)
+        )
+        _watchaParty.value = listOf(
+            WatchaPartyItem(title = "왕과 사는 남자", time = "21:13", imageRes = R.drawable.img_king_man),
+            WatchaPartyItem(title = "파묘", time = "22:22", imageRes = R.drawable.img_grave_digging)
+        )
+    }
+}
