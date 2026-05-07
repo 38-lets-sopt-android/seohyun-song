1a95a488031b33c57b9aecb22ab32c30cec9a90d
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-05-08 01:35:56 +0900

    feat: #12 SignUpScreen 스크롤 가능하게 조정

--- app/src/main/java/com/example/letssopt/presentation/signup/SignUpScreen.kt
+++ app/src/main/java/com/example/letssopt/presentation/signup/SignUpScreen.kt
@@ -1,6 +1,7 @@
 package com.example.letssopt.presentation.signup
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,8 +10,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
@@ -44,95 +47,98 @@ fun SignUpScreen(
     onSignUpClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Column(
+    Box(
         modifier = modifier
             .fillMaxSize()
             .background(Color(0xFF141414))
-            .padding(horizontal = 20.dp)
-            .navigationBarsPadding(),
+            .navigationBarsPadding()
     ) {
-        Text(
-            text = "watcha",
-            fontSize = 36.sp,
-            fontWeight = FontWeight.Bold,
-            fontFamily = FontFamily(Font(R.font.pretendard_bold)),
-            color = Color(0xFFE8003C),
+        Column(
             modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .padding(top = 60.dp),
-        )
+                .fillMaxSize()
+                .padding(horizontal = 20.dp)
+                .verticalScroll(rememberScrollState())
+                .padding(bottom = 78.dp)
+        ) {
+            Text(
+                text = "watcha",
+                fontSize = 36.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily(Font(R.font.pretendard_bold)),
+                color = Color(0xFFE8003C),
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 60.dp),
+            )
 
-        Text(
-            text = "회원가입",
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            fontFamily = FontFamily(Font(R.font.pretendard_bold)),
-            color = Color.White,
-            modifier = Modifier
-                .align(Alignment.Start)
-                .padding(top = 26.dp),
-        )
+            Text(
+                text = "회원가입",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily(Font(R.font.pretendard_bold)),
+                color = Color.White,
+                modifier = Modifier
+                    .align(Alignment.Start)
+                    .padding(top = 26.dp),
+            )
 
-        Spacer(modifier = Modifier.height(36.dp))
+            Spacer(modifier = Modifier.height(36.dp))
 
-        SignUpTextField(
-            label = "아이디",
-            value = uiState.loginIdInput,
-            onValueChange = onLoginIdChange,
-            placeholder = "아이디를 입력하세요",
-        )
+            SignUpTextField(
+                label = "아이디",
+                value = uiState.loginIdInput,
+                onValueChange = onLoginIdChange,
+                placeholder = "아이디를 입력하세요",
+            )
 
-        SignUpTextField(
-            label = "비밀번호",
-            value = uiState.pwInput,
-            onValueChange = onPasswordChange,
-            placeholder = "비밀번호를 입력하세요",
-            visualTransformation = PasswordVisualTransformation()
-        )
+            SignUpTextField(
+                label = "비밀번호",
+                value = uiState.pwInput,
+                onValueChange = onPasswordChange,
+                placeholder = "비밀번호를 입력하세요",
+                visualTransformation = PasswordVisualTransformation()
+            )
 
-        SignUpTextField(
-            label = "비밀번호 확인",
-            value = uiState.pwConfirm,
-            onValueChange = onPasswordConfirmChange,
-            placeholder = "비밀번호를 다시 입력하세요",
-            visualTransformation = PasswordVisualTransformation()
-        )
+            SignUpTextField(
+                label = "비밀번호 확인",
+                value = uiState.pwConfirm,
+                onValueChange = onPasswordConfirmChange,
+                placeholder = "비밀번호를 다시 입력하세요",
+                visualTransformation = PasswordVisualTransformation()
+            )
 
-        SignUpTextField(
-            label = "이름",
-            value = uiState.nameInput,
-            onValueChange = onNameChange,
-            placeholder = "이름을 입력하세요"
-        )
+            SignUpTextField(
+                label = "이름",
+                value = uiState.nameInput,
+                onValueChange = onNameChange,
+                placeholder = "이름을 입력하세요"
+            )
 
-        SignUpTextField(
-            label = "이메일",
-            value = uiState.emailInput,
-            onValueChange = onEmailChange,
-            placeholder = "이메일을 입력하세요"
-        )
+            SignUpTextField(
+                label = "이메일",
+                value = uiState.emailInput,
+                onValueChange = onEmailChange,
+                placeholder = "이메일을 입력하세요"
+            )
 
-        SignUpTextField(
-            label = "나이",
-            value = uiState.ageInput,
-            onValueChange = onAgeChange,
-            placeholder = "나이를 입력하세요",
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-        )
+            SignUpTextField(
+                label = "나이",
+                value = uiState.ageInput,
+                onValueChange = onAgeChange,
+                placeholder = "나이를 입력하세요",
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
 
-        SignUpTextField(
-            label = "파트",
-            value = uiState.partInput,
-            onValueChange = onPartChange,
-            placeholder = "파트를 입력하세요"
-        )
-
-        Spacer(modifier = Modifier.weight(1f))
+            SignUpTextField(
+                label = "파트",
+                value = uiState.partInput,
+                onValueChange = onPartChange,
+                placeholder = "파트를 입력하세요"
+            )
+        }
 
         Button(
-            onClick = {
-                onSignUpClick()
-            },
+            onClick = { onSignUpClick() },
             enabled = uiState.isButtonEnabled,
             shape = RoundedCornerShape(8.dp),
             colors = ButtonDefaults.buttonColors(
@@ -143,8 +149,10 @@ fun SignUpScreen(
             ),
             modifier = Modifier
                 .fillMaxWidth()
+                .padding(horizontal = 20.dp)
                 .padding(bottom = 26.dp)
                 .heightIn(min = 52.dp)
+                .align(Alignment.BottomCenter)
         ) {
             Text(
                 "회원가입",
