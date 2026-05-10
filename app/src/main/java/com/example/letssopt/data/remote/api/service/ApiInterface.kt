85b3d165b3b9cb32c3170862626ffa26a059735a
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-05-10 16:54:57 +0900

    feat: #14 [심화 과제] UserDTO 만들기 및 interface에 API 추가

--- app/src/main/java/com/example/letssopt/data/remote/api/service/ApiInterface.kt
+++ app/src/main/java/com/example/letssopt/data/remote/api/service/ApiInterface.kt
@@ -1,12 +1,15 @@
 package com.example.letssopt.data.remote.api.service
 
+import com.example.letssopt.data.remote.dto.GetUserResponse
 import com.example.letssopt.data.remote.dto.LoginRequest
 import com.example.letssopt.data.remote.dto.LoginResponse
 import com.example.letssopt.data.remote.dto.SignUpRequest
 import com.example.letssopt.data.remote.dto.SignUpResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface ApiInterface {
     @POST("api/v1/auth/signup")
@@ -14,4 +17,9 @@ interface ApiInterface {
 
     @POST("api/v1/auth/signin")
     suspend fun signIn(@Body request: LoginRequest): Response<LoginResponse>
+
+    @GET("/api/v1/users/{userId}")
+    suspend fun getUser(
+        @Path("userId") userId: Int
+    ): GetUserResponse
 }
